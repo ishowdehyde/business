f2fd82a9bfb9c43c2ad83ae76d3a034b27c20b83
ishowdehyde <165624526+ishowdehyde@users.noreply.github.com>
2024-11-19 18:38:23 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const whatsappButon = document.querySelector("whasapp-button");
+
+  whatsappButon.addEventListener("click", () => {
+    alert("You are being redirected to Whatsapp. Feel free to chat with us!");
+  });
+});
